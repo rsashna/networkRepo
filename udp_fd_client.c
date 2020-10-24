20a8a190a07f662cf4806b973fac214409df6d7f
rsashna <rashna@ryerson.ca>
2020-10-24 16:56:22 -0400

    added user quit app functionality

--- udp_fd_client.c
+++ udp_fd_client.c
@@ -27,11 +27,13 @@ main(int argc, char **argv)
 	char	*host = "localhost";
 	int	port = 3000;
 	char	now[100];		/* 32-bit integer to hold time	*/ 
-	struct hostent	*phe;	/* pointer to host information entry	*/
+	struct hostent	*phe;		/* pointer to host information entry	*/
 	struct sockaddr_in sin;	/* an Internet endpoint address		*/
-	int	s, n, type;	/* socket descriptor and socket type	*/
+	int	s, n, type;		/* socket descriptor and socket type	*/
 int 	sd;
 char	*bp, rbuf[BUFLEN], sbuf[BUFLEN];
+struct  pdu{char type; char data[100];}; 	/* pdu 101 bytes with first byte is type  */
+
 FILE *cFile;
 	switch (argc) {
 	case 1:
@@ -70,21 +72,50 @@ FILE *cFile;
 
 //while(connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0){
 
+
+while(1){	/* get user message */ /*READ*/
+
+char fileToD[30];
+struct pdu spdu;
+
+
+printf("\n\n--------------------------------------------------------- \n");
+printf("--------------------------------------------------------- \n");
 printf("------------- UDP FILE DOWNLOAD APPLICATION ------------- \n");
 printf("--------------------------------------------------------- \n");
-printf("Name the file to download: \n");
+printf("--------------------------------------------------------- \n\n");
+printf("Enter Q to quit download application, or \n");
+printf("Name the file to download: \n\n");
 
-while(n=read(0, sbuf, BUFLEN)){	/* get user message */ /*READ*/
-char fileToD[30];
+//n=read(0, sbuf, BUFLEN);
+//n=read(0, sbuf, BUFLEN);
+
+
+
+spdu.type= 'C';		//Set the type to FILENAME PDU
+n=read(0, spdu.data, 100); 	//Read the filename entered by the user
+spdu.data[n-1] = '\0';		//End file name str
+
+if(spdu.data[0]=='Q'){		//Let user quit application
+	//fclose(cFile);
+	//close(sd);
+	exit(1);
+}
+
+
+memcpy(fileToD, spdu.data, strlen(spdu.data));	/*create a file with same name*/
+write(sd, &spdu, n+1);		//Send the PDU to the serv
+
+
 //fileToD=sbuf;
-memcpy(fileToD, "c" + sbuf, strlen(sbuf));	/*create a file with same name*/
-fileToD[strlen(sbuf)]='\0';
-//printf("Checking sever for: %s", sbuf);
+//memcpy(fileToD, sbuf, strlen(sbuf));	/*create a file with same name*/
+//fileToD[strlen(sbuf)]='\0';
+printf("Checking sever for: %s", spdu.data);
 
-write(sd, sbuf, BUFLEN);
+//write(sd, sbuf, BUFLEN);
 	  
-	  read(sd, sbuf, BUFLEN);
-	  fprintf(stdout, "File request %s\n", sbuf); /*prints found or error*/
+////	  read(sd, sbuf, BUFLEN);
+////	  fprintf(stdout, "File request %s\n", sbuf); /*prints found or error*/
 	  
 cFile = fopen(fileToD, "w");
 
@@ -95,33 +126,21 @@ if (cFile == NULL) {
 }
 
 fprintf(stdout, "\n-----FILE CONTENTS Start-----\n");
-read(sd, sbuf, strlen(sbuf));
+//read(sd, sbuf, strlen(sbuf));
 //while(sbuf[0]=='D' || sbuf[0]=='F'){ /*gets more than 100c until eof*/
-while(sbuf[0]=='D' || sbuf[0]=='F'){ /*gets more than 100c until eof*/
+while(read(sd, sbuf, strlen(sbuf))){ /*gets more than 100c until eof*/
 fprintf(stdout, "%s", sbuf);/*print to stdout*/
 fprintf(cFile, "%s", sbuf);/*print to file*/
 }
 fprintf(stdout, "\n---------FILE END--------\n\n");
 	
 	fclose(cFile);
-	close(sd);
-	return(0);
+	
 	}
 
 
 //}
 
-
-
-
-
-//	(void) write(s, MSG, strlen(MSG));
-
-	/* Read the time */
-
-//	n = read(s, (char *)&now, sizeof(now));
-//	if (n < 0)
-//		fprintf(stderr, "Read failed\n");
-//	write(1, now, n);
+	close(sd);
 	exit(0);
 }
