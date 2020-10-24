19da9728ee705e046f7b07776722a811f9b522b2
rsashna <rashna@ryerson.ca>
2020-10-24 07:45:56 -0400

    client WIP

--- udp_fd_client.c
+++ udp_fd_client.c
@@ -1,4 +1,4 @@
-/* time_client.c - main */
+/* UDP FILE DOWNLOADER FROM SERVER */
 
 #include <sys/types.h>
 
@@ -13,7 +13,7 @@
 #include <netdb.h>
 
 #define	BUFSIZE 64
-
+#define BUFLEN		101	/* buffer length */
 #define	MSG		"Any Message \n"
 
 
@@ -30,7 +30,9 @@ main(int argc, char **argv)
 	struct hostent	*phe;	/* pointer to host information entry	*/
 	struct sockaddr_in sin;	/* an Internet endpoint address		*/
 	int	s, n, type;	/* socket descriptor and socket type	*/
-
+int 	sd;
+char	*bp, rbuf[BUFLEN], sbuf[BUFLEN];
+FILE *cFile;
 	switch (argc) {
 	case 1:
 		break;
@@ -57,23 +59,69 @@ main(int argc, char **argv)
 		fprintf(stderr, "Can't get host entry \n");
                                                                                 
     /* Allocate a socket */
-        s = socket(AF_INET, SOCK_DGRAM, 0);
+        s = socket(AF_INET, SOCK_DGRAM, 0);				/*SOCKET*/
         if (s < 0)
 		fprintf(stderr, "Can't create socket \n");
 	
                                                                                 
     /* Connect the socket */
-        if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+        if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)	/*BIND*/
 		fprintf(stderr, "Can't connect to %s %s \n", host, "Time");
 
+//while(connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0){
 
-	(void) write(s, MSG, strlen(MSG));
+printf("------------- UDP FILE DOWNLOAD APPLICATION ------------- \n");
+printf("--------------------------------------------------------- \n");
+printf("Name the file to download: \n");
+
+while(n=read(0, sbuf, BUFLEN)){	/* get user message */ /*READ*/
+char fileToD[30];
+//fileToD=sbuf;
+memcpy(fileToD, "c" + sbuf, strlen(sbuf));	/*create a file with same name*/
+fileToD[strlen(sbuf)]='\0';
+//printf("Checking sever for: %s", sbuf);
+
+write(sd, sbuf, BUFLEN);
+	  
+	  read(sd, sbuf, BUFLEN);
+	  fprintf(stdout, "File request %s\n", sbuf); /*prints found or error*/
+	  
+cFile = fopen(fileToD, "w");
+
+if (cFile == NULL) {
+  fprintf(stderr, "Can't open output file %s!\n",
+          fileToD);
+  exit(1);
+}
+
+fprintf(stdout, "\n-----FILE CONTENTS Start-----\n");
+read(sd, sbuf, strlen(sbuf));
+//while(sbuf[0]=='D' || sbuf[0]=='F'){ /*gets more than 100c until eof*/
+while(sbuf[0]=='D' || sbuf[0]=='F'){ /*gets more than 100c until eof*/
+fprintf(stdout, "%s", sbuf);/*print to stdout*/
+fprintf(cFile, "%s", sbuf);/*print to file*/
+}
+fprintf(stdout, "\n---------FILE END--------\n\n");
+	
+	fclose(cFile);
+	close(sd);
+	return(0);
+	}
+
+
+//}
+
+
+
+
+
+//	(void) write(s, MSG, strlen(MSG));
 
 	/* Read the time */
 
-	n = read(s, (char *)&now, sizeof(now));
-	if (n < 0)
-		fprintf(stderr, "Read failed\n");
-	write(1, now, n);
+//	n = read(s, (char *)&now, sizeof(now));
+//	if (n < 0)
+//		fprintf(stderr, "Read failed\n");
+//	write(1, now, n);
 	exit(0);
 }
